a9602c4722b18ff39ffa03e5f9710c3cf16c99bd
tlarram <tlarramore@gmail.com>
2022-10-14 15:24:07 -0400

    basic spring file set up commit

---
+++ sidewalksale/src/test/java/com/triplethreat/sidewalksale/SidewalksaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triplethreat.sidewalksale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidewalksaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
