27227af313921c390addb781fe14eeeff89213df
S-Slater <107280570+S-Slater@users.noreply.github.com>
2022-10-17 15:33:15 -0400

    Added GeoLocation features
    Added simple geolocation features in with some controllers, models, jsp files.

---
+++ sidewalksale/src/main/java/com/triplethreat/sidewalksale/models/GeoIP.java
@@ -0,0 +1,60 @@
+package com.triplethreat.sidewalksale.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="locations")
+public class GeoIP {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String ipAddress;
+    private String city;
+    private String latitude;
+    private String longitude;
+    
+    public GeoIP () {}
+
+	public GeoIP(String ipAddress, String city, String latitude, String longitude) {
+		this.ipAddress = ipAddress;
+		this.city = city;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	public String getIpAddress() {
+		return ipAddress;
+	}
+
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	};
+    
+}
---
+++ sidewalksale/src/main/java/com/triplethreat/sidewalksale/services/GeoIPService.java
@@ -0,0 +1,50 @@
+package com.triplethreat.sidewalksale.services;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+
+import org.springframework.stereotype.Service;
+
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.CityResponse;
+import com.triplethreat.sidewalksale.models.GeoIP;
+
+@Service
+public class GeoIPService {
+
+	
+	private DatabaseReader dbReader;
+	
+	 public GeoIPService() throws IOException {
+	        File database = new File("C:\\Users\\samue\\OneDrive\\Desktop\\STSOCT2022\\GeoLocationbd\\GeoLite2-City.mmdb");
+	        dbReader = new DatabaseReader.Builder(database).build();
+	    }
+	 public GeoIP getLocation(String ip) 
+		      throws IOException, GeoIp2Exception {
+		        InetAddress ipAddress = InetAddress.getByName(ip);
+		        CityResponse response = dbReader.city(ipAddress);
+		        
+		        String cityName = response.getCity().getName();
+		        String latitude = 
+		          response.getLocation().getLatitude().toString();
+		        String longitude = 
+		          response.getLocation().getLongitude().toString();
+		        return new GeoIP(ip, cityName, latitude, longitude);
+	}
+	 
+	//public GeoIP saveLocation(String ip) throws IOException, GeoIp2Exception {
+	//	        InetAddress ipAddress = InetAddress.getByName(ip);
+	//	        CityResponse response = dbReader.city(ipAddress);
+	//	        
+	//	        String cityName = response.getCity().getName();
+	//	        String latitude = 
+	//	          response.getLocation().getLatitude().toString();
+	//	        String longitude = 
+	//	          response.getLocation().getLongitude().toString();
+	//	        GeoIP newGeoIP = new GeoIP(ip, cityName, latitude, longitude);
+	//	        return GeoRepo.save(newGeoIP);
+	// }
+	 
+}
