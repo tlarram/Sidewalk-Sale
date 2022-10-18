50516c41b5c04b0bc7f348b996b699b35dbe1a23
S-Slater <107280570+S-Slater@users.noreply.github.com>
2022-10-18 17:29:15 -0400

    Geolocation to the home page and bigfixes

--- sidewalksale/src/main/java/com/triplethreat/sidewalksale/models/GeoIP.java
+++ sidewalksale/src/main/java/com/triplethreat/sidewalksale/models/GeoIP.java
@@ -1,16 +1,7 @@
 package com.triplethreat.sidewalksale.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name="locations")
 public class GeoIP {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
 	private String ipAddress;
     private String city;
     private String latitude;
